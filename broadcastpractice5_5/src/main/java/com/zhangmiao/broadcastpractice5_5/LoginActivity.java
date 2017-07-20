9c0e83932f6bc17996cb25bc3cb6cf24b23767ae
zhangmiao <zhangmiao.dream@qq.com>
2017-07-20 14:48:39 +0800

    增加记住账号和密码功能。

--- broadcastpractice5_5/src/main/java/com/zhangmiao/broadcastpractice5_5/LoginActivity.java
+++ broadcastpractice5_5/src/main/java/com/zhangmiao/broadcastpractice5_5/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.zhangmiao.broadcastpractice5_5;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -20,13 +24,30 @@ public class LoginActivity extends BaseActivity {
 
     private Button login;
 
+    private SharedPreferences pref;
+
+    private SharedPreferences.Editor editor;
+
+    private CheckBox rememberPass;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
         accountEdit = (EditText) findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
+        rememberPass = (CheckBox) findViewById(R.id.remember_pass);
         login = (Button) findViewById(R.id.login);
+        boolean isRemember = pref.getBoolean("remember password", false);
+        if (isRemember) {
+            //将账号和密码都设置到文本框中
+            String accout = pref.getString("account", "");
+            String password = pref.getString("password", "");
+            accountEdit.setText(accout);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,6 +55,15 @@ public class LoginActivity extends BaseActivity {
                 String password = passwordEdit.getText().toString();
                 //如果账号是admin且密码是123456，就认为登录成功
                 if (account.equals("admin") && password.equals("123456")) {
+                    editor = pref.edit();
+                    if (rememberPass.isChecked()) {//检查复选框是否被选中
+                        editor.putBoolean("remember password", true);
+                        editor.putString("account", account);
+                        editor.putString("password", password);
+                    } else {
+                        editor.clear();
+                    }
+                    editor.commit();
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
