8fd8f4147988c942a83a8f3fe350419909dd1d53
zhangmiao <zhangmiao.dream@qq.com>
2017-07-26 21:08:26 +0800

    8.4的视频与音频代码完成。

---
+++ playaudiotest8_4/src/main/java/com/zhangmiao/playaudiotest8_4/MainActivity.java
@@ -0,0 +1,72 @@
+package com.zhangmiao.playaudiotest8_4;
+
+import android.media.MediaPlayer;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button play;
+    private Button pause;
+    private Button stop;
+
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        play = (Button) findViewById(R.id.play);
+        pause = (Button) findViewById(R.id.pause);
+        stop = (Button) findViewById(R.id.stop);
+        initMediaPlayer(); //初始化MediaPlayer
+    }
+
+    private void initMediaPlayer() {
+        try {
+            File file = new File(Environment.getExternalStorageDirectory(), "music.mp3");
+            mediaPlayer.setDataSource(file.getPath());//指定音频文件的路径
+            mediaPlayer.prepare();//让MediaPlayer进入到准备状态
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.play:
+                if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.start(); //开始播放
+                }
+                break;
+            case R.id.pause:
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();//暂停播放
+                }
+                break;
+            case R.id.stop:
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.reset(); //停止播放
+                    initMediaPlayer();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+    }
+}
---
+++ playvideotest8_4/src/main/java/com/zhangmiao/playvideotest8_4/MainActivity.java
@@ -0,0 +1,71 @@
+package com.zhangmiao.playvideotest8_4;
+
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private VideoView videoView;
+
+    private Button play;
+
+    private Button pause;
+
+    private Button replay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        play = (Button) findViewById(R.id.play);
+        pause = (Button) findViewById(R.id.pause);
+        replay = (Button) findViewById(R.id.replay);
+        videoView = (VideoView) findViewById(R.id.video_view);
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        replay.setOnClickListener(this);
+        initVideoPath();
+    }
+
+    private void initVideoPath() {
+        File file = new File(Environment.getExternalStorageDirectory(), "movio.3gp");
+        videoView.setVideoPath(file.getPath()); //指定视频文件的路径
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.play:
+                if (!videoView.isPlaying()) {
+                    videoView.start(); //开始播放
+                }
+                break;
+            case R.id.pause:
+                if (videoView.isPlaying()) {
+                    videoView.pause(); //暂时播放
+                }
+                break;
+            case R.id.replay:
+                if (videoView.isPlaying()) {
+                    videoView.resume(); //重新播放
+                }
+                break;
+        }
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (videoView != null) {
+            videoView.suspend();//将VideoView所占用的资源释放掉
+        }
+    }
+}
