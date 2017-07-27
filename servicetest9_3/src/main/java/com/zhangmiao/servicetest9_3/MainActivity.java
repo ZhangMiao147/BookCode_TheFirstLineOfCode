f3878aaea6e588971b508a420a884eb345e5f74b
zhangmiao <zhangmiao.dream@qq.com>
2017-07-27 17:23:00 +0800

    9.3的ServiceTest使用服务代码完成。

---
+++ servicetest9_3/src/main/java/com/zhangmiao/servicetest9_3/MainActivity.java
@@ -0,0 +1,71 @@
+package com.zhangmiao.servicetest9_3;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button startService;
+    private Button stopService;
+    private Button bindService;
+    private Button unbindService;
+
+    private MyService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            downloadBinder = (MyService.DownloadBinder)iBinder;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startService = (Button) findViewById(R.id.start_service);
+        stopService = (Button) findViewById(R.id.stop_service);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+
+        bindService = (Button) findViewById(R.id.bind_service);
+        unbindService = (Button) findViewById(R.id.unbind_service);
+        bindService.setOnClickListener(this);
+        unbindService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.start_service:
+                Intent startIntent = new Intent(this, MyService.class);
+                startService(startIntent); //启动服务
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this, MyService.class);
+                stopService(stopIntent); //停止服务
+                break;
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this, MyService.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE);//绑定服务，BIND_AUTO_CREATE表示在活动和服务进行绑定后自动创建服务，service的oncreate会执行，onStartCommand不会执行
+                break;
+            case R.id.unbind_service:
+                unbindService(connection);//解绑服务
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ servicetest9_3/src/main/java/com/zhangmiao/servicetest9_3/MyService.java
@@ -0,0 +1,54 @@
+package com.zhangmiao.servicetest9_3;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Author: zhangmiao
+ * Date: 2017/7/27
+ */
+public class MyService extends Service {
+
+    private static final String TAG = MyService.class.getSimpleName();
+
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder {
+        public void startDownload() {
+            Log.d(TAG, "startDownloaded executed");
+        }
+
+        public int getProgress() {
+            Log.d(TAG, "getProgress executed");
+            return 0;
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate executed");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onstartCommand executed");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestory executed");
+    }
+}
