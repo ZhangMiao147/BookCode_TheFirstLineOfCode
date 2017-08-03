355ffe9727d33f354c2f850f22045e3c493fe536
zhangmiao <zhangmiao.dream@qq.com>
2017-08-03 14:24:02 +0800

    12.4的使用方向传感器代码完成。

---
+++ compasstest12_4/src/main/java/com/zhangmiao/compasstest12_4/MainActivity.java
@@ -0,0 +1,81 @@
+package com.zhangmiao.compasstest12_4;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private SensorManager sensorManager;
+
+    private ImageView compassImg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        compassImg = (ImageView) findViewById(R.id.compass_img);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor magneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Sensor accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(listener, magneticSensor, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(listener, accelerometerSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (sensorManager != null) {
+            sensorManager.unregisterListener(listener);
+        }
+    }
+
+    private SensorEventListener listener = new SensorEventListener() {
+
+        float[] accelerometerValues = new float[3];
+        float[] magneticValues = new float[3];
+
+        private float lastRotateDegree;
+
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            //判断当前是加速度传感器还是地磁传感器
+            if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                //注意赋值时要调用clone()方法
+                accelerometerValues = sensorEvent.values.clone();
+            } else if (sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                //注意赋值时要调用clone()方法
+                magneticValues = sensorEvent.values.clone();
+            }
+            float[] R = new float[9];
+            float[] values = new float[3];
+            SensorManager.getRotationMatrix(R, null, accelerometerValues, magneticValues);
+            SensorManager.getOrientation(R, values);
+            //将计算出的旋转角度取反，用于旋转指南针背景图
+            float rotateDegree = -(float) Math.toDegrees(values[0]);
+            if (Math.abs(rotateDegree - lastRotateDegree) > 1) {
+                RotateAnimation animation = new RotateAnimation(lastRotateDegree, rotateDegree, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                animation.setFillAfter(true);
+                compassImg.startAnimation(animation);
+                lastRotateDegree = rotateDegree;
+            }
+            Log.d(TAG, "value[0] is " + Math.toDegrees(values[0]));
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+}
